aa63dd4330ea30ec631f058c1e080581f395bd01
“Madeline <mehanley2@gmail.com>
2024-04-17 17:25:53 -0400

    add weather api

--- react-ladies/src/App.tsx
+++ react-ladies/src/App.tsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [weatherData, setWeatherData] = useState(null)
+  const [city, setCity] = useState('')
+  const apiKey = "d2a7283109fd4b498da222355241004";
+  const getWeather = async() => {
+    try {
+      const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`,
+      {
+        method: "POST",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "*"
+      }
+    }
+      )
+      return await res.json();
 
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  useEffect(() => {
+    getWeather().then(res => {
+      setWeatherData(res);
+    })
+  },[city])
+  const debounce = (callback, wait=500) => {
+    let timeoutId = null;
+    return (...args) => {
+      window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback(...args);
+      }, wait);
+    };
+  }
   return (
     <>
       <div>
@@ -15,6 +49,10 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div>
+      <input type="text" value={city} onChange={(e) => debounce(setCity(e.target.value))} />
+      <div>
+        {JSON.stringify(weatherData)}
       </div>
       <h1>Vite + React</h1>
       <div className="card">
